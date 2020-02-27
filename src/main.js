674a6e895a91742bbf90dcaa7bd03183abb632d0
Karoly Gossler <connor@connor.hu>
2020-02-27 09:10:01 +0100

    hide unstable functions

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ var usuariumMenu;
 
 function onOpen() {
   SpreadsheetApp.getUi().createMenu('Usuarium')
-    .addSubMenu(SpreadsheetApp.getUi().createMenu('Calendar')
-      .addItem('Validate Calendar', 'validateCalendar')
-      .addItem('Add Rubrum column and fill it', 'calendarAddRubrum'))
+    // .addSubMenu(SpreadsheetApp.getUi().createMenu('Calendar')
+    //   .addItem('Validate Calendar', 'validateCalendar')
+    //   .addItem('Add Rubrum column and fill it', 'calendarAddRubrum'))
     .addSubMenu(SpreadsheetApp.getUi().createMenu('Index')
       .addItem('Validate', 'IndexValidatorStartValidation'))
-    .addSubMenu(SpreadsheetApp.getUi().createMenu('System 3')
-      .addItem('Validate and Fill', 'System3ValidatorStartValidation'))
+    // .addSubMenu(SpreadsheetApp.getUi().createMenu('System 3')
+    //   .addItem('Validate and Fill', 'System3ValidatorStartValidation'))
     .addToUi();
 }
